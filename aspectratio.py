b4d5f9aeeb8062e9b076b35e813c5a0835fd0967
K Kollmann <git@k.kollmann.moe>
2015-02-22 02:31:53 +0100

    finish rewriting the program...

--- aspectratio.py
+++ aspectratio.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 '''
-A program to (re)calculate an image's dimensions
+A \nProgram to (re)calculate an image's dimensions
 when one of its sides is changed in size.
 
 Copyright (c) 2015 K Kollmann <code∆k.kollmann·moe>
@@ -12,8 +12,23 @@ License: http://opensource.org/licenses/MIT The MIT License (MIT)
 import math
 import sys
 
-def calculate_newdimension(w,h):
-    ''' Recalculate the dimensions of an image based on
+def recalculate_dimensions(old_w, old_h, w=0, h=0):
+    new_w = w
+    new_h = h
+
+    try:
+        float(new_w)
+    except TypeError:
+        # height provided, calculate new width
+        new_w = (old_w * float(new_h)) / old_h
+    else:
+        # width provided, calculate new height
+        new_h = (old_h * float(new_w)) / old_w
+
+    return new_w, new_h
+
+def provide_dimensions(w,h):
+    ''' recalculate_dimensions the dimensions of an image based on
         current width, current height, and either a new width or height.
     '''
     global old_w
@@ -23,30 +38,69 @@ def calculate_newdimension(w,h):
         float(h)
     except ValueError:
         if not old_w and not old_h:
-            print("There was at least one invalid value.")
+            print("You entered at least one invalid value - try again.")
             return None, None
         else:
             # analyse new value(s) provided
-            print("2nd loop -- do stuff here ") #debug
-            return w, h
+            try:
+                float(w)
+            except ValueError:
+                if w == '':
+                    try:
+                        float(h)
+                    except ValueError:
+                        pass
+                    except KeyboardInterrupt:
+                        sys.exit("\nProgram aborted by user.")
+                    # try-except-else
+                    else:
+                        # width is empty
+                        new_w, new_h = recalculate_dimensions(old_w, old_h, None, float(h))
+                        return new_w, new_h
+            except KeyboardInterrupt:
+                sys.exit("\nProgram aborted by user.")
+            # try-except-else
+            else:
+                try:
+                    float(h)
+                except ValueError:
+                    if h == '':
+                        # height is empty
+                        new_w, new_h = recalculate_dimensions(old_w, old_h, float(w), None)
+                        return new_w, new_h
+                except KeyboardInterrupt:
+                    sys.exit("\nProgram aborted by user.")
+    except KeyboardInterrupt:
+        sys.exit("\nProgram aborted by user.")
+    # try-except-else
     else:
         # set variables for original width and height
         if not old_w and not old_h:
-            old_w = w
-            old_h = h
+            old_w = float(w)
+            old_h = float(h)
 
     while True:
         print("Input new value of (only) one of the dimensions:")
-        new_w, new_h = calculate_newdimension(input("new w: "), input("new h: "))
+        new_w, new_h = provide_dimensions(input("new w: "), input("new h: "))
         return new_w, new_h
         break
 
+    return new_w, new_h
+
 old_w = None
 old_h = None
+new_w = None
+new_h = None
 
 if __name__ == "__main__":
-    while True:
-        print("Current dimensions of your image:")
-        new_w, new_h = calculate_newdimension(input("w: "), input("h: "))
-        print("new width: {}\nnew height: {}" .format(new_w, new_h))
-        break
+    try:
+        while True:
+            print("Current dimensions of your image:")
+            new_w, new_h = provide_dimensions(input("w: "), input("h: "))
+            if new_w is not None:
+                print("-----")
+                print("new width: {}\nnew height: {}" .format(new_w, new_h))
+                break
+    except KeyboardInterrupt:
+        sys.exit("\nProgram aborted by user.")
+
