a6529356121f103f457d55e02c74e4e455300788
K Kollmann <git@k.kollmann.moe>
2016-03-26 02:22:03 +0100

    apple notification on script failure

--- timelogger.py
+++ timelogger.py
@@ -5,6 +5,7 @@ import tl_config as tlconf
 import logging
 import time
 from os.path import expanduser
+import subprocess
 
 rn = time.strftime("%Y-%m-%d %H:%M")
 home = expanduser("~")
@@ -14,7 +15,10 @@ logging.basicConfig(filename='error.log', level=logging.DEBUG,
 logger = logging.getLogger(__name__)
 
 try:
-    thisfile = open(tlconf.user_dir + "logger.txt", 'a')
-    thisfile.write("logged: " + rn + " (home: " + home + ")\n")
+    this_file = open(tlconf.user_dir + "logger.txt", 'a')
+    this_file.write("logged: " + rn + " (home: " + home + ")\n")
 except Exception as err:
+    subprocess.call('osascript -e "display notification \\\"$(date)\n' + err + '\\\" '
+                    'with title \\\"Timelogger script failed!\\\" "',
+                    shell=True)
     logger.error(err)
