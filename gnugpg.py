9f4c172a68dd0117fe0340c29f74078355d3369d
K Kollmann <git@k.kollmann.moe>
2016-03-30 11:40:24 +0200

    create keys, list keys

--- gnugpg.py
+++ gnugpg.py
@@ -3,13 +3,18 @@
 
 import gnupg as gnupg
 
-gpg = gnupg.GPG()
-print(gpg)
-
 # check for existence of GnuPG on local system
 try:
-    gpg = gnupg.GPG()
+    gpg = gnupg.GPG(verbose='True')
+    gpg.encoding = 'ascii'
     print(gpg)
+    # input_data = gpg.gen_key_input(key_type='RSA',
+    #                               key_length=1024, name_real='Test name')
+    # key = gpg.gen_key(input_data)
+    # print(key)
+    public_keys = gpg.list_keys()
+    print(public_keys)
+
 except RuntimeError as err:
     print(err)
     print("You need to download and install GnuPG before "
