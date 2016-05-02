62a26e435caedf0dba9ae9fb005a6b8ed0919b3f
K Kollmann <git@k.kollmann.moe>
2016-05-02 15:14:00 +0200

    shift error logging

--- pythonied/timelogger.py
+++ pythonied/timelogger.py
@@ -5,19 +5,21 @@ import timelogger_config as tlconf
 import logging
 import time
 from os.path import expanduser
+from sys import path
+from os.path import join
 import subprocess
 
 rn = time.strftime("%Y-%m-%d %H:%M")
 home = expanduser("~")
 
-logging.basicConfig(filename='error.log', level=logging.DEBUG,
-                    format='%(asctime)s %(levelname)s %(name)s %(message)s')
-logger = logging.getLogger(__name__)
-
 try:
-    this_file = open(tlconf.user_dir + tlconf.log_file, 'a')
+    this_file = open(join(tlconf.user_dir, tlconf.log_file), 'a')
     this_file.write("logged: " + rn + " (home: " + home + ")\n")
 except Exception as err:
+    # create error log in directory containing the script
+    logging.basicConfig(filename=join(path[0],tlconf.error_log), level=logging.DEBUG,
+                    format=tlconf.error_format)
+    logger = logging.getLogger(__name__)
     subprocess.call('osascript -e "display notification '
                     '\\\"$(date)\n' + str(err) + '\\\" '
                     'with title \\\"Timelogger script failed!\\\" "',
