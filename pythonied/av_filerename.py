b4276a2d2c2a2692dc0b3ac211e50c125fc6624c
K Kollmann <git@k.kollmann.moe>
2016-06-02 14:31:26 +0200

    file types tuple instead of list(s), match file types

--- pythonied/av_filerename.py
+++ pythonied/av_filerename.py
@@ -1,8 +1,29 @@
 #! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2016 K Kollmann <code∆k.kollmann·moe>
+
+import av_filerename_config as config
+import os
+
 
 def main():
-    video_files = ['mpg', 'mpeg', 'avi', 'mkv', 'mp4']
-    additional_files = ['srt', 'idx', 'sub']
+    """
+    Main function.
+    """
+    # ---VARS---
+    filetypes = ('mpg', 'mpeg', 'avi', 'mkv', 'mp4', 'srt', 'idx', 'sub')
+    search_dirs = config.DIRS
+
+    files = []
+
+    for this_dir in search_dirs:
+        for startdir, subdirs, filenames in os.walk(this_dir):
+            for file in filenames:
+                if file.endswith(filetypes):
+                    files.append(file)
+
+    print(files)
 
 if __name__ == "__main__":
     main()
