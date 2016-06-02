f8055bc742f59b92440b311342acc802f9512f7e
K Kollmann <git@k.kollmann.moe>
2016-06-02 18:50:57 +0200

    add func description, formatting, var names

--- pythonied/av_filerename.py
+++ pythonied/av_filerename.py
@@ -6,31 +6,41 @@
 # Rename video and audio files.
 
 
-import av_filerename_config as config
+import filerename_config as config
 import os
 
 
 def find_files_bytype(this_dir, filetypes):
+    """
+    Find files of a certain file type in a given directory
+    (and its subdirectories).
+
+    :param this_dir: the directory to start the search at
+    :param filetypes: a tuple of allowed file extensions
+    :return: a list of all file names
+    """
     file_list = []
-    for startdir, subdirs, filenames in os.walk(this_dir):
-        for file in filenames:
+    for directory, subdir, filename in os.walk(this_dir):
+        for file in filename:
             if file.endswith(filetypes):
                 file_list.append(file)
     return file_list
+
 
 def main():
     """
     Main function.
     """
     # ---VARS---
-    filetypes = config.FTYPES # list
-    search_dirs = config.DIRS # list
+    filetypes = config.FTYPES  # list
+    search_dirs = config.DIRS  # list
 
     files = []
 
     for this_dir in search_dirs:
         files.extend(find_files_bytype(this_dir, tuple(filetypes)))
 
+    # debug
     print(files)
 
 if __name__ == "__main__":
