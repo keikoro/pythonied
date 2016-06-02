e3ef3fd7a2105c49b3b7085203d5d41b94ed2c00
K Kollmann <git@k.kollmann.moe>
2016-06-02 02:21:46 +0200

    add file types to keep

--- pythonied/av_filerename.py
+++ pythonied/av_filerename.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python3
 
 def main():
+    video_files = ['mpg', 'mpeg', 'avi', 'mkv', 'mp4']
+    additional_files = ['srt', 'idx', 'sub']
 
 if __name__ == "__main__":
     main()
