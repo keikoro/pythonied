965fba19a75df2314e1c29112d01aa2a768f589c
K Kollmann <git@k.kollmann.moe>
2016-06-03 17:07:51 +0200

    formatting

--- setup.py
+++ setup.py
@@ -1,21 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import os
 from setuptools import setup, Command
+
 
 class cleanup(Command):
     """Get rid of unneeded files after running setup.py."""
     user_options = []
+
     def initialize_options(self):
         pass
+
     def finalize_options(self):
         pass
+
     def run(self):
         os.system("rm -rfv ./build ./dist ./*.pyc ./*.tgz ./*.egg-info")
+
 
 setup(
     name='pythonied',
     version='1.0',
     author='K Kollmann',
-    author_email='code∆kerstinkollmann· com',
+    author_email='code∆k.kollmann·moe',
     license='',
     classifiers=[
         'Development Status :: 3 - Alpha',
@@ -24,9 +32,9 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
     ],
-    keywords='misc., scripts',
-
-    cmdclass = {
+    keywords=['misc.', 'scripts'],
+    cmdclass={
         'clean': cleanup
-    }, requires=['python-gnupg']
+    },
+    requires=['python-gnupg']
 )
