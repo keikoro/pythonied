58e8934da25eaeb9b2d540d5a4392a3ca85b4904
K Kollmann <git@k.kollmann.moe>
2016-06-15 03:00:15 +0200

    match pattern, save results in dict, write to file

--- pythonied/searchdict.py
+++ pythonied/searchdict.py
@@ -20,13 +20,42 @@ def main():
     # ---VARS---
     dicts = conf.DICTS
     words = conf.WORDS
-    countw = len(words)
-    matches_single = []
-    matches_all = []
+    matches = {}
+    patterns = []
 
+    # create a pattern (object) based on each word
+    for w in words:
+        matches[w] = {}
+        p = '[\w]*{}[\w]*'.format(w)
+        p_obj = re.compile(p, flags=re.IGNORECASE)
+        patterns.append(p_obj)
 
-    for match in matches_single:
-        print(match)
+    for d in dicts:
+        print("Searching dictionary {}".format(d))
+        with open(d, mode='r') as file:
+            # iterate over all lines, stripping newlines
+            for line in file.read().splitlines():
+                # ignore comments in dictionary files
+                if not line.startswith("#"):
+                    for w, p in zip(words, patterns):
+                        this_match = p.search(line)
+                        # put all matches for a word into
+                        # the dictionary named after the word
+                        if this_match:
+                            matches[w][this_match.group(0)] = ''
+        file.close()
+
+    for m, words in matches.items():
+        # write all results into a file based on the word
+        result = open('results_' + m + '.txt', 'w')
+        result.write('---\n' + m + '\n---\n')
+        # debug
+        # print(m)
+        # print('---')
+        for w in words:
+            result.write(w + '\n')
+            # debug
+            # print(w)
 
 
 if __name__ == "__main__":
