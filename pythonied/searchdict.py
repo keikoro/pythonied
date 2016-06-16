438c4b5f78e39b7e15c105b2175905fbcd561311
K Kollmann <git@k.kollmann.moe>
2016-06-16 02:10:46 +0200

    put output files in var, only run script if dicts, words exist

--- pythonied/searchdict.py
+++ pythonied/searchdict.py
@@ -11,6 +11,9 @@
 
 import re
 import config as conf
+from os.path import dirname, realpath, join, isdir
+from os import makedirs
+import sys
 
 
 def main():
@@ -23,39 +26,51 @@ def main():
     matches = {}
     patterns = []
 
-    # create a pattern (object) based on each word
-    for w in words:
-        matches[w] = {}
-        p = '[\w]*{}[\w]*'.format(w)
-        p_obj = re.compile(p, flags=re.IGNORECASE)
-        patterns.append(p_obj)
-
-    for d in dicts:
-        print("Searching dictionary {}".format(d))
-        with open(d, mode='r') as file:
-            # iterate over all lines, stripping newlines
-            for line in file.read().splitlines():
-                # ignore comments in dictionary files
-                if not line.startswith("#"):
-                    for w, p in zip(words, patterns):
-                        this_match = p.search(line)
-                        # put all matches for a word into
-                        # the dictionary named after the word
-                        if this_match:
-                            matches[w][this_match.group(0)] = ''
-        file.close()
-
-    for m, words in matches.items():
-        # write all results into a file based on the word
-        result = open('results_' + m + '.txt', 'w')
-        result.write('---\n' + m + '\n---\n')
-        # debug
-        # print(m)
-        # print('---')
+    if dicts is not None and words is not None:
+        # create a pattern (object) based on each word
         for w in words:
-            result.write(w + '\n')
+            matches[w] = {}
+            p = '[\w]*{}[\w]*'.format(w)
+            p_obj = re.compile(p, flags=re.IGNORECASE)
+            patterns.append(p_obj)
+
+        for d in dicts:
+            print("Searching dictionary {}".format(d))
+            with open(d, mode='r') as file:
+                # iterate over all lines, stripping newlines
+                for line in file.read().splitlines():
+                    # ignore comments in dictionary files
+                    if not line.startswith("#"):
+                        for w, p in zip(words, patterns):
+                            this_match = p.search(line)
+                            # put all matches for a word into
+                            # the dictionary named after the word
+                            if this_match:
+                                matches[w][this_match.group(0)] = ''
+            file.close()
+
+        # save output files in /var/ directory
+        this_dir = dirname(realpath(sys.argv[0]))
+        output_dir = join(dirname(this_dir), 'var')
+
+        if not isdir(output_dir):
+            try:
+                makedirs(output_dir)
+            except Exception as err:
+                print("Couldn't create directory for saving files. Exiting.")
+                exit(1)
+
+        # write all results into a file based on the word
+        for m, words in matches.items():
+            result = open(join(output_dir, 'results_' + m + '.txt'), 'w')
+            result.write('---\n' + m + '\n---\n')
             # debug
-            # print(w)
+            # print(m)
+            # print('---')
+            for w in words:
+                result.write(w + '\n')
+                # debug
+                # print(w)
 
 
 if __name__ == "__main__":
