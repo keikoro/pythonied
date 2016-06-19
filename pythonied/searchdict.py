ebb79501bbeedf81f00a258c6772097d01418e03
K Kollmann <git@k.kollmann.moe>
2016-06-19 22:07:03 +0200

    save intersecting matches in separate function

--- pythonied/searchdict.py
+++ pythonied/searchdict.py
@@ -16,28 +16,34 @@ import re
 import argparse
 from itertools import combinations
 from locale import setlocale, getlocale, strxfrm, LC_ALL
-from functools import cmp_to_key
 
-# strxfrm apparently broken for German UTF-8
+# strxfrm apparently broken for German UTF-8 on OS X
 # -> umlauts get sorted after words starting with z
 setlocale(LC_ALL, 'de_AT.UTF-8')
+
 
 # debug
 # print(getlocale(LC_ALL))
 
 
-def print_results(word, matches):
+def print_results(word, match_dict):
     """
     Print all matches found for a word to stdout.
 
     :param word: a word that was looked for
-    :param matches: dictionary of matches for that word
+    :param match_dict: dictionary of matches for that word
     """
     # create 'headline'
     sep = '-' * len(word)
     print(sep)
     print(str(word))
     print(sep)
+
+    # account for intersected word sets
+    if isinstance(match_dict, set):
+        matches = {str(match_dict): ''}
+    else:
+        matches = dict(match_dict)
 
     # use lambda for sorting the matches lowercased
     # (otherwise matches starting with a capital letter get sorted first)
@@ -48,31 +54,21 @@ def print_results(word, matches):
     print()
 
 
-def create_dir(output_dir):
-    try:
-        makedirs(output_dir)
-    # TODO narrow exception clause
-    except Exception as err:
-        print("Couldn't create directory for "
-              "saving files. Exiting.")
-        print(err)
-        exit(1)
-
-
 def intersect_matches(match_dict):
     """
     Intersect all existing result sets for words.
 
     :param match_dict: dictionary of dictionaries of matches
-    :return:
+    :return: a dictionary of dictionaries containing matches
     """
     shared = {}
 
     matches = dict(match_dict.items())
-    words = list(sorted(match_dict.keys()))
+    words = list(sorted(match_dict.keys(), key=str.lower))
 
     for i in range(2, len(words) + 1):
         for elems in combinations(words, i):
+
             # list of sets of matches
             match_sets = [set(list(matches[elems[j]])) for j in
                           range(len(elems))]
@@ -87,13 +83,13 @@ def intersect_matches(match_dict):
     return shared
 
 
-def save_matches(output_dir, word, matches):
+def save_single(output_dir, word, match_dict):
     """
     Save found matches into text files.
 
     :param output_dir: directory to save files in
     :param word: the word which was searched for
-    :param matches: a dictionary of matches
+    :param match_dict: a dictionary of matches
     """
     # file name pattern results_WORD.txt
     filename = join(output_dir, 'results_' + word + '.txt')
@@ -101,13 +97,18 @@ def save_matches(output_dir, word, matches):
     # create 'headline'
     sep = '-' * len(word)
     file = open(filename, 'w')
-    file.write(sep)
+    file.write(word + '\n')
+    file.write(sep + '\n')
+
+    matches = dict(match_dict)
 
     # use lambda for sorting the matches lowercased
     # (otherwise matches starting with a capital letter get sorted first)
-    # strxfrm for umlaut sorting broken in several German locals
+    # strxfrm for umlaut sorting broken in some German locales on OS X?
     for word, suppl in sorted(matches.items(),
-                              key=lambda w: strxfrm(w[0].lower())):
+                              key=lambda w: (
+                                      strxfrm(w[0].lower()),
+                                      strxfrm(w[1].lower()))):
         file.write(word)
         if suppl:
             file.write('\t[' + suppl + ']')
@@ -117,6 +118,41 @@ def save_matches(output_dir, word, matches):
         # if value:
         #     print('\t[' + value + ']', end='')
     # print()
+
+    file.close()
+
+
+def save_intersecting(output_dir, words, match_dict):
+    """
+     Save intersectnig matches into one single text file.
+
+    :param output_dir: directory to save file in
+    :param words: the word combinations which were searched for
+    :param match_dict: a dictionary of matches
+    """
+    filename = join(output_dir, 'shared_' + '+'.join(
+        sorted(words, key=str.lower)) + '.txt')
+
+    file = open(filename, 'w')
+
+    # use lambda for sorting the matches lowercased
+    # (otherwise matches starting with a capital letter get sorted first)
+    # strxfrm for umlaut sorting broken in some German locales on OS X?
+    matches = dict(match_dict)
+    for combo, results in sorted(matches.items(),
+                                 key=lambda w: (
+                                         strxfrm(''.join(w[0]).lower()),
+                                         strxfrm(''.join(w[1]).lower()))):
+
+        # separator
+        combo_combined = '+'.join(combo)
+        sep = '-' * len(combo_combined)
+        file.write(combo_combined + '\n')
+        file.write(sep + '\n')
+
+        for res in results:
+            file.write(res + '\n')
+        file.write('\n')
 
     file.close()
 
@@ -177,6 +213,17 @@ def create_patterns(words, patterns):
         patterns.append([p_obj, pfull_obj])
 
     return patterns
+
+
+def create_dir(output_dir):
+    try:
+        makedirs(output_dir)
+    # TODO narrow exception clause
+    except Exception as err:
+        print("Couldn't create directory for "
+              "saving files. Exiting.")
+        print(err)
+        exit(1)
 
 
 def main():
@@ -240,7 +287,7 @@ def main():
             # function call per word
             for word, results in matches.items():
                 if save:
-                    save_matches(output_dir, word, results)
+                    save_single(output_dir, word, results)
                     print("Search for '{}' saved: {} "
                           "matches!".format(word, len(results)))
                 else:
@@ -248,12 +295,20 @@ def main():
 
             # intersect matches to find words that match sets have in common
             if intersect:
+                print("Looking for shared words in all matches...")
                 shared_matches = intersect_matches(matches)
-                if save:
-                    print(shared_matches)
+                if shared_matches:
+                    if save:
+                        save_intersecting(output_dir, words, shared_matches)
+                        print("Shared words saved.")
+                    else:
+                        for word_combo, results in sorted(
+                                shared_matches.items(),
+                                key=lambda w: (strxfrm(
+                                            ''.join(list(w[0])).lower()))):
+                            print_results('+'.join(word_combo), results)
                 else:
-                    print(shared_matches)
-
+                    print("No intersections found.")
         else:
             print("No matches.")
 
